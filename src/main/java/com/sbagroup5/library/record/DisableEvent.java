29aa0bd26676ea7991a9531ce51ac9943ddff851
Liu2k5 <truongcuc05@gmail.com>
2026-06-14 11:28:52 +0700

    delete unnecessary imports

--- src/main/java/com/sbagroup5/library/record/DisableEvent.java
+++ src/main/java/com/sbagroup5/library/record/DisableEvent.java
@@ -1,8 +1,4 @@
 package com.sbagroup5.library.record;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
 import java.io.Serializable;
 
 public record DisableEvent(String username) implements Serializable {
