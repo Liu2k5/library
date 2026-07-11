1e96b31e73d79a06e57b87862f186e3c899827fd
zdusgxbhjt <tjhbxgsudz100@gmail.com>
2026-07-11 22:32:36 +0700

    Fix unique collumn of user entity, implements serializable for role entity

--- backend/src/main/java/com/sbagroup5/library/entity/user/User.java
+++ backend/src/main/java/com/sbagroup5/library/entity/user/User.java
@@ -4,15 +4,22 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity
@@ -20,7 +27,10 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_users_email", columnNames = "email"),
+        @UniqueConstraint(name = "uk_users_phone", columnNames = "phone")
+})
 public class User implements UserDetails {
     @Id
     private String username;
@@ -31,13 +41,13 @@ public class User implements UserDetails {
     @Column(columnDefinition = "NVARCHAR(100)")
     private String fullName;
 
-    @Column(columnDefinition = "VARCHAR(100) UNIQUE NOT NULL")
+    @Column(columnDefinition = "VARCHAR(100)", nullable = false)
     private String email;
 
     @Column(columnDefinition = "VARCHAR(64)")
     private String password;
 
-    @Column(columnDefinition = "VARCHAR(20) UNIQUE NOT NULL")
+    @Column(columnDefinition = "VARCHAR(20)", nullable = false)
     private String phone;
 
     @Column(columnDefinition = "NVARCHAR(255)")
@@ -61,5 +71,4 @@ public class User implements UserDetails {
     public String getUsername() {
         return username;
     }
-
 }
