2f7524164bef9edb63400e61c13f5043be138095
hienenenenen <dinhhien1652005@gmail.com>
2026-07-14 12:54:15 +0700

    Them Router cho librarian

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,27 @@ import Dashboard from "./components/admin/pages/Dashboard";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="/librarian/books" element={<BookList />} />
+      <Route path="/librarian/books/add" element={<BookForm />} />
+      <Route path="/librarian/books/edit/:id" element={<BookForm />} />
+
+      <Route path="/librarian/categories" element={<CategoryList />} />
+      <Route path="/librarian/categories/add" element={<CategoryForm />} />
+      <Route path="/librarian/categories/edit/:id" element={<CategoryForm />} />
+
+      <Route path="/librarian/authors" element={<AuthorList />} />
+      <Route path="/librarian/authors/add" element={<AuthorForm />} />
+      <Route path="/librarian/authors/edit/:id" element={<AuthorForm />} />
+
+      <Route path="/librarian/books/:bookId/copies" element={<BookCopyList />} />
+      <Route path="/librarian/books/:bookId/copies/add" element={<BookCopyForm />} />
+      <Route path="/librarian/bookcopies/edit/:id" element={<BookCopyForm />} />
+    </Routes>
   );
 }
 
